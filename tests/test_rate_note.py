6ced6dff231ea9df9db6580c88f6e98fee447a92
fronre <islamhala35@gmail.com>
2025-11-12 16:22:36 +0100

    Red(test rate 8 returns unsuccessful)

--- tests/test_rate_note.py
+++ tests/test_rate_note.py
@@ -1,8 +1,8 @@
 from src.rate_note import rate_note
 
 
-
-
+def test_rate_8_returns_unsuccessful():
+     assert rate_note(8) == "unsuccessful"
 
 def test_rate_9_returns_unsuccessful():
          assert rate_note(9) == "unsuccessful"
